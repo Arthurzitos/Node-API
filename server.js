f326991ffddcb55b58126a0ce234e16a2a2ea220
Arthurzitos <arthurgruimaraes@gmail.com>
2025-08-26 00:48:24 -0300

    criando servidor local e com fastify

--- server.js
+++ server.js
@@ -1,8 +1,34 @@
-import {createServer} from 'node:http';
+// Criando server nativo com Node puro
 
-const server = createServer (() => {
-    response.write('Servidor rodando!');
-    return response.end();
-})
+// import {createServer} from 'node:http';
 
-server.listen(3333);
+// const server = createServer ((request, response) => {
+//     response.write('Servidor rodando!');
+//     return response.end();
+// })
+
+// server.listen(3333);
+
+// ------------------------------------------>
+
+// Criando server utilizando o Framework Fastify:
+
+import { fastify } from 'fastify';
+
+const server = fastify();
+
+server.get('/', () => {
+    return 'Hello World!';
+});
+
+server.get('/hello', () => {
+    return 'Hello User!';
+});
+
+server.get('/node', () => {
+    return 'Hello Node.js!';
+});
+
+server.listen({
+    port: 3333,
+});
